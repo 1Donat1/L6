c5a93dfc3e4d57d7ad22f4354262d41aa3afdad4
1Donat1 <79876158+1Donat1@users.noreply.github.com>
2022-05-30 00:15:32 +0300

    Update main.js

--- 4/script/main.js
+++ 4/script/main.js
@@ -1,17 +1,17 @@
 'use strict'
 const liter=+prompt('Введите количество литров  борща')
+
 function potatoCounter( liters ){
+
     const potatoWeight = 0.075;
     const numberPotato = 4;
     const priceForKillo = 13;
     let totalPotatoKillos = null;
     let totalPrice = null;
     
-
     if(!liters){
        return  alert('error');
     }
-
 
 else{
     let totalPotatoAmount = numberPotato * liters;
@@ -20,8 +20,6 @@ else{
     return alert(`Цена за ${totalPotatoKillos} кг картошки ${totalPrice} грн`)
   
 }
-
-
 
 }
 potatoCounter(liter);
